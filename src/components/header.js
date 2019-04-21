36deed5f13f073253a31ccd14e3560d2233a2ed6
unknown <joseacm1989@gmail.com>
2019-04-20 21:58:29 -0400

    original commit

---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './header.css'
+const Header = props => {
+  return (
+    <header className="header">
+      <div className="nav-links">
+        <a href="#1"> Home </a>
+        <a href="#1"> Porfafolio </a>
+        <a href="#1"> About </a>
+        <a href="#1"> Contact </a>
+      </div>
+    </header>
+  )
+}
+
+export default Header
