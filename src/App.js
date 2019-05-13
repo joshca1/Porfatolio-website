a02861903664157da4957d271da138cd42bdf32a
unknown <joseacm1989@gmail.com>
2019-05-12 20:55:27 -0400

    fixing navigations, adding styles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react'
+import React, { useState } from 'react'
 import Header from './components/header'
 import Content from './components/content'
 
@@ -7,12 +7,10 @@ import ActiveBurger from './burger-context.js'
 const App = () => {
   const [burger, setBurger] = useState({ isActive: false })
   return (
-    <Fragment>
-      <ActiveBurger.Provider value={{ burger, setBurger }}>
-        <Header />
-        <Content />
-      </ActiveBurger.Provider>
-    </Fragment>
+    <ActiveBurger.Provider value={{ burger, setBurger }}>
+      <Header />
+      <Content />
+    </ActiveBurger.Provider>
   )
 }
 
--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './about.scss'
 const About = props => {
   return (
-    <div className="about">
+    <div className="about" id="about">
       <h1>Front End Developer</h1>
       <p>Javascript developer 1 year experience</p>
     </div>
