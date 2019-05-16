591a3e8c6042cf4e176655eb21fa0ff45f3958a2
unknown <joseacm1989@gmail.com>
2019-05-16 15:07:29 -0400

    adding footer contact component

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -4,7 +4,13 @@ const About = props => {
   return (
     <div className="about" id="about">
       <h1>Front End Developer</h1>
-      <p>Javascript developer 1 year experience</p>
+      <p>
+        Selft taught Developer 1 year + experience passionate for learning new
+        tecnologies, atention to details and seeking new challenges.
+      </p>
+      <a className="link-portafolio" href="#portafolio">
+        {''}
+      </a>
     </div>
   )
 }
