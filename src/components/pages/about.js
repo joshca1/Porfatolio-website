f2a6174397ef49351232eafbdc1990f5583eeb30
unknown <joseacm1989@gmail.com>
2019-05-16 18:51:51 -0400

    adding compresed about image

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -6,8 +6,14 @@ const About = props => {
       <h1>Front End Developer</h1>
       <p>
         Selft taught Developer 1 year + experience passionate for learning new
-        tecnologies, atention to details and seeking new challenges.
+        tecnologies and seeking new challenges.
       </p>
+      <p>
+        I started 4 years ago in programing, making small code changes from a
+        existing software
+      </p>
+      <p>since then my interest for software has just grown over years.</p>
+      <p />
       <a className="link-portafolio" href="#portafolio">
         {''}
       </a>
