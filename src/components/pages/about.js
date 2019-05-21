bd5dbbd874103e94cee32eeb89c5807f73d40a8f
unknown <joseacm1989@gmail.com>
2019-05-20 22:06:30 -0400

    adding cv link to header

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -5,15 +5,15 @@ const About = props => {
     <div className="about" id="about">
       <h1>Front End Developer</h1>
       <p>
-        Selft taught Developer 1 year + experience passionate for learning new
-        tecnologies and seeking new challenges.
+        Hello, i am Jose Cañizales Selft taught programer from Venezuela
+        actually using React, Vue tecnologies with 1 year + experience
+        passionate for learning new tecnologies.
+        <br />
+        <br /> I am looking for a junior Front-end or full stack position i am
+        up for new challenges. I started 4 years ago in programming, making
+        small code changes from a existing software since then my interest for
+        software has just grown over years.
       </p>
-      <p>
-        I started 4 years ago in programing, making small code changes from a
-        existing software
-      </p>
-      <p>since then my interest for software has just grown over years.</p>
-      <p />
       <a className="link-portafolio" href="#portafolio">
         {''}
       </a>
